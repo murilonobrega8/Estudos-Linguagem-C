88b4513ca15473f0d87947de7900f9f7244a9220
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-17 17:03:50 -0300

    Create PrimeirosEstudos.c

---
+++ PrimeirosEstudos.c
@@ -0,0 +1,13 @@
+//Função principal do programa
+int main() {
+    
+    //Imprime na Tela
+    printf("Olá, Mundo!");
+
+    //Pausa o programa após a execução
+    system("pause");
+
+    //Retorna o valor 0
+    return 0;
+
+}
