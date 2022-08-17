1906dccf3834fe01570f02946dc8af9f7512ad31
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-17 17:28:03 -0300

    Concatenando

--- ImprimirLerConcatenar.c
+++ ImprimirLerConcatenar.c
@@ -6,8 +6,17 @@ void main() {
     //Imprimindo a variável "a"
     printf("%d", a);
 
+    //Concatenando
+    printf("O valor da variável 'a' é: %d", a);
+
     //Pula linha
     printf("\n\n");
+
+    //Mudando o valor de "a"
+    a = a+10;
+
+    //Concatenando
+    printf("O valor da variável 'a' somado a 10 é: %d", a);
 
     //Imprimindo
     printf("\nUhuuu");
