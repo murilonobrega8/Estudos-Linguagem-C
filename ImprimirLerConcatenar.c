53541400a4dd1d5d59bcb39679bde663c5f69640
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-17 17:21:13 -0300

    Imprimir e Ler

---
+++ ImprimirLerConcatenar.c
@@ -0,0 +1,18 @@
+void main() {
+
+    //Definindo uma variável
+    int a = 5;
+
+    //Imprimindo a variável "a"
+    printf("%d", a);
+
+    //Pula linha
+    printf("\n\n");
+
+    //Imprimindo
+    printf("\nUhuuu");
+
+    //Pausando
+    system("pause");    
+
+}
