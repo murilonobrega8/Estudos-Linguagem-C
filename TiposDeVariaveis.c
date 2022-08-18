84c99f46b9fd980bf1e24f73287bea35ce44fbc4
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-17 21:45:24 -0300

    update

---
+++ TiposDeVariaveis.c
@@ -0,0 +1,28 @@
+void main() {
+
+    //Definindo uma variável
+    int     a;
+    float   b;
+    char    c;
+
+    //Passando Valores
+    a = 5;
+    b = 2.3;
+    c = ;
+
+    //Escrevendo na Tela
+    printf("\nO valor da variável 'a' é: %d", a);
+    printf("\nO valor da variável 'b' é: %.2f", b);
+
+    //Lendo um valor
+    scanf("%d", &a);
+    scanf("%f", &b);
+
+    //Escrevendo na Tela
+    printf("\nO valor da variável 'a' é: %d", a);
+    printf("\nO valor da variável 'b' é: %.2f", b);
+
+    //Pausando
+    system("pause");    
+
+}
