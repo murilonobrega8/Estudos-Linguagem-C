eac75c6ed01caafaa934b33a75219950e47b80ca
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-18 18:49:21 -0300

    Create Operações Matemáticas.c

---
+++ Operações Matemáticas.c
@@ -0,0 +1,52 @@
+
+//Função Principal do Programa
+void main() {
+
+    //Definindo variáveis
+    int     vrv1, vrv2;
+
+    //Soma
+    vrv1 = 5;
+    vrv2 = 3;
+
+    //Escrevendo na Tela
+    printf("\n A soma da variável %d com a variável %d é: %d", vrv1, vrv2, vrv1 + vrv2);
+
+    //Subtração
+    vrv1 = 5;
+    vrv2 = 3;
+
+    //Escrevendo na Tela
+    printf("\n A subtração da variável %d com a variável %d é: %d", vrv1, vrv2, vrv1 - vrv2);
+
+    //Divisão
+    vrv1 = 6;
+    vrv2 = 3;
+
+    //Escrevendo na Tela
+    printf("\n A divisão da variável %d pela variável %d é: %d", vrv1, vrv2, vrv1 / vrv2);
+
+    //Multiplicação
+    vrv1 = 6;
+    vrv2 = 3;
+
+    //Escrevendo na Tela
+    printf("\n A multiplicação da variável %d pela variável %d é: %d", vrv1, vrv2, vrv1 * vrv2);
+
+    //Resto da Divisão
+    vrv1 = 6;
+    vrv2 = 3;
+
+    //Escrevendo na Tela
+    printf("\n O resto da Divisão da variável %d pela variável %d é: %d", vrv1, vrv2, vrv1 % vrv2);
+
+    //Valor Absoluto
+    vrv1 = -6;
+
+    //Escrevendo na Tela
+    printf("\n O Valor Absoluto da variável %d é: %d", vrv1, abs(vrv1);
+
+    //Pausando o Programa Após Executar
+    system("pause");    
+
+}
