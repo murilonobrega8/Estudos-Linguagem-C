b443624801561e89e4b5944eac07f30f77c2575c
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-18 14:00:54 -0300

    Update TiposDeVariaveis.c

--- TiposDeVariaveis.c
+++ TiposDeVariaveis.c
@@ -1,26 +1,29 @@
 void main() {
 
     //Definindo uma variável
-    int     a;
-    float   b;
-    char    c;
+    int     vrv1;
+    float   vrv2;
+    char    vrv3;
 
     //Passando Valores
-    a = 5;
-    b = 2.3;
-    c = ;
+    vrv1 = 5;
+    vrv2 = 2.3;
+    vrv3 = 'teste';
 
     //Escrevendo na Tela
-    printf("\nO valor da variável 'a' é: %d", a);
-    printf("\nO valor da variável 'b' é: %.2f", b);
+    printf("\nO valor da variável 'vrv1' é: %d", vrv1);
+    printf("\nO valor da variável 'vrv2' é: %.2f", vrv2);
+    printf("\nO valor da variável 'vrv3' é: %c\n", vrv3);
 
     //Lendo um valor
-    scanf("%d", &a);
-    scanf("%f", &b);
+    scanf("%d", &vrv1);
+    scanf("%f", &vrv2);
+    scanf(" %c", &vrv3);
 
     //Escrevendo na Tela
-    printf("\nO valor da variável 'a' é: %d", a);
-    printf("\nO valor da variável 'b' é: %.2f", b);
+    printf("\nO valor informado da variável 'vrv1' é: %d", vrv1);
+    printf("\nO valor informado da variável 'vrv2' é: %.2f", vrv2);
+    printf("\nO valor informado da variável 'vrv3' é: %c\n", vrv3);
 
     //Pausando
     system("pause");    
