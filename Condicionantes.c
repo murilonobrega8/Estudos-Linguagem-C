6776ab418426c1cccc5cc1303ac1b2484345bb7a
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-19 20:10:33 -0300

    Create Condicionantes.c

---
+++ Condicionantes.c
@@ -0,0 +1,21 @@
+
+//Função Principal do Programa
+void main() {
+
+    //Definindo variáveis
+    int     vrv1;
+
+    //Condicional
+    vrv1 = 5;
+    
+    //Escrevendo na Tela
+    if(vrv1 == 5) {
+        printf("\n A variável 'vrv1' é: %d", vrv1);
+    }
+
+    // 
+
+    //Pausando o Programa Após Executar
+    system("pause");    
+
+}
