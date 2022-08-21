c6e6e1b481968619a21e67719ed6723402a35571
Murilo Oliveira <murilonobrega8@gmail.com>
2022-08-21 20:37:36 -0300

    Update PrimeirosEstudos.c

--- PrimeirosEstudos.c
+++ PrimeirosEstudos.c
@@ -1,5 +1,8 @@
-//Função principal do programa
-int main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(void){
     
     //Imprime na Tela
     printf("Olá, Mundo!");
